5efc1611f5af64c5e86f8f79ef3b40817893ebf3
“larooly“ <rmawltjs0812@naver.com>
2021-12-10 16:38:53 +0900

    체크 빠진게 있었다

--- kkk/CanvasViewController.h
+++ kkk/CanvasViewController.h
@@ -10,7 +10,22 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface CanvasViewController : UIViewController
+@interface CanvasViewController : UIViewController{
+    float backX;
+    float backY;
+    float nowX;
+    float nowY;
+    NSTimer *ClearNow;
+   
+}
+@property (nonatomic,assign) float newRed;
+@property (nonatomic,assign) float newGreen;
+@property (nonatomic,assign) float newBlue;
+@property (nonatomic,assign) float newPensize;
+@property (strong, nonatomic) IBOutlet UIView *totalview;
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImage;
+- (void)Testremove;
+
 
 @end
 
--- kkk/MainViewController.h
+++ kkk/MainViewController.h
@@ -10,8 +10,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UIImagePickerControllerDelegate,UINavigationControllerDelegate>{
+    NSTimer *UpdatePenSetting;
+    UIImagePickerController *imagePickerController;
+}
+- (IBAction)TrashCanClick:(id)sender;
+@property (weak, nonatomic) IBOutlet UIView *MainCanvaslo;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *ShowNowPenSet;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *PenSeet;
+//@property (weak, nonatomic) IBOutlet UIImageView *TestImageAlbum;
+- (IBAction)ChangePenandEraser:(id)sender;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *buttonPenEraser;
 
+- (IBAction)Sharebutton:(id)sender;
+@property (weak, nonatomic) IBOutlet UITableView *sendTableView;
+@property(strong,nonatomic)NSArray *SendData;
 @end
 
 NS_ASSUME_NONNULL_END
