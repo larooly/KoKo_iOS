d9b24c041ad2aea73c9b9ff4f3f56be61d8c65af
“larooly“ <rmawltjs0812@naver.com>
2021-12-10 14:47:17 +0900

    first commit

---
+++ kkk/MainViewController.h
@@ -0,0 +1,17 @@
+//
+//  MainViewController.h
+//  kkk
+//
+//  Created by active on 2020/05/27.
+//  Copyright © 2020 activeKJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ kkk/PenSettingViewController.h
@@ -0,0 +1,17 @@
+//
+//  PenSettingViewController.h
+//  kkk
+//
+//  Created by active on 2020/05/27.
+//  Copyright © 2020 activeKJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PenSettingViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
